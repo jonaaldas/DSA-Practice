568b672a1380b729d656293c90751ddd66269441
Jonathan Aldas <jonaaldas@gmail.com>
2022-10-07 20:14:23 -0500

    added fizz buzz

---
+++ Exercices/Arrays/FizzBuzz/app.js
@@ -0,0 +1,1 @@
+// Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, "buzz" at multiples of 5 and "fizzbuzz" at multiples of 3 and 5
--- Exercices/Strings/Valid Palindrome/app.js
+++ Exercices/Strings/Valid Palindrome/app.js
@@ -2,27 +2,26 @@
 
 // Given a string s, return true if it is a palindrome, or false otherwise.
 
-
 // Input: s = "A man, a plan, a canal: Panama"
 // Output: true
 // Explanation: "amanaplanacanalpanama" is a palindrome.
 // s = "A man, a plan, a canal: Panama"
 // s = "raceacar"
-s = 'ccdadcc'
+s = 'ccdadcc';
 const isPalindrome = function (s) {
-  s = s.replace(/[^a-z0-9]/gi, '').toLowerCase()
-  let leftIndex = 0
-  let rightIndex = s.length - 1
-  while (leftIndex < rightIndex) {
-    if (s[leftIndex] !== s[rightIndex]) {
-      return false
-    }
-    leftIndex++
-    rightIndex--
-  }
-  return true
+	s = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+	let leftIndex = 0;
+	let rightIndex = s.length - 1;
+	while (leftIndex < rightIndex) {
+		if (s[leftIndex] !== s[rightIndex]) {
+			return false;
+		}
+		leftIndex++;
+		rightIndex--;
+	}
+	return true;
 };
 
 // Time Complexity O[N]
 // Space Complexity O[1]
-console.log(isPalindrome(s))
+console.log(isPalindrome(s));
